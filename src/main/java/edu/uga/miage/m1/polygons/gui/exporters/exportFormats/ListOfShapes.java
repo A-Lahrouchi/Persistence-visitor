e7453c9ca6d667415244087b80eb4f02fb73be05
lahrouchi.pro@gmail.com <lahrouchi.pro@gmail.com>
2023-11-20 00:03:14 +0100

    simple refactoring

--- src/main/java/edu/uga/miage/m1/polygons/gui/exporters/exportFormats/ListOfShapes.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/exporters/exportFormats/ListOfShapes.java
@@ -1,11 +1,8 @@
 package edu.uga.miage.m1.polygons.gui.exporters.exportFormats;
 
-import edu.uga.miage.m1.polygons.gui.shapes.SimpleShape;
 
 public interface ListOfShapes {
 
     public void pushToShapeList(String shape);
-
-//    public SimpleShape popFromShapeList(); 
 
 }
---
+++ src/main/java/edu/uga/miage/m1/polygons/gui/shapes/groupeShapes.java
@@ -0,0 +1,5 @@
+package edu.uga.miage.m1.polygons.gui.shapes;
+
+public class groupeShapes {
+    
+}
